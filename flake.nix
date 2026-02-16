5bb4b99e1ece6a7b1b7f83c981282ea2d708a049
nazozokc <nazozokc@gmail.com>
2026-02-16 17:11:07 +0900

    nix-updater-bot add to flake.nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Nix auto update bot (Deno + TS)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            deno
+            nvim
+            git
+            nix
+          ];
+
+          shellHook = ''
+            echo "🚀 Deno Nix Bot Dev Environment"
+            deno --version
+          '';
+        };
+      }
+    );
+}
+
